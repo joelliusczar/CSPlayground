d7123f764e55e0f12013c12ec89b145aa93a759a
joelliusp <joelliuscaesar@gmail.com>
2018-02-09 15:47:42 -0500

    added new project

---
+++ ManVsCars/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManVsCars
+{
+    public class Starter: System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
