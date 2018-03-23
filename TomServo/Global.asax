59bcd576df8e3cd23c00ee1cc5bf0569225c9544
joelliusp <joelliuscaesar@gmail.com>
2018-03-23 16:59:48 -0400

    Another test web project

---
+++ TomServo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TomServo.MvcApplication" Language="C#" %>
---
+++ TomServo/StNull.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TomServo
+{
+    public class StNull
+    {
+        public string NullString { get; set; }
+    }
+}
