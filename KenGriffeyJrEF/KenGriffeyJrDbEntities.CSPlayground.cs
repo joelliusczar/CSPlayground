df69310b082b39838b0f5d817ba2e3ca150ecda9
joelliusp <joelliuscaesar@gmail.com>
2018-06-02 16:13:49 -0400

    deleting old dbs

---
+++ KenGriffeyJrEF/KenGriffeyJrDbEntities.CSPlayground.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace KenGriffeyJrEF
+{
+    public partial class KenGriffeyJrDbEntities
+    {
+        public static KenGriffeyJrDbEntities GetNew()
+        {
+            KenGriffeyJrDbEntities instance = new KenGriffeyJrDbEntities();
+            instance.SetupInterceptors();
+            return instance;
+        }
+
+        private void SetupInterceptors()
+        {
+            DbInterception.Add(new EFInterception());
+        }
+    }
+}
